bd42b64fc65d0c47ec512c7af6609cc3507c8b60
Lalo <86909609+la-lo-go@users.noreply.github.com>
2023-01-11 22:50:43 +0100

    hello there, git!

---
+++ src/scripts/blur.js
@@ -0,0 +1,90 @@
+const episodeSelectorClass = ".episodeSelector-container"; // Div of epidodes
+const titleCardListClass = ".titleCardList--container"; // Div of each episode
+
+const div_episodeSelector = document.querySelector(episodeSelectorClass);
+let blurEnabled = false;
+
+chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
+  console.log(message);
+  
+  if (message.message === "blur status") {
+    blurEnabled = message.value;
+
+    if (blurEnabled) {
+      blur();
+    }
+  }
+});
+
+const observerBody = new MutationObserver(() => {
+
+  if (!div_episodeSelector) {
+    return;
+  }
+
+  blur();
+
+  let observerEpisodes = new MutationObserver(() => {
+    blur();
+  });
+
+  observerEpisodes.observe(div_episodeSelector, {
+    childList: true,
+    subtree: true,
+  });
+});
+
+observerBody.observe(document.body, {
+  childList: true,
+  subtree: true,
+});
+
+function blur() {
+  const episodeSelector = document.querySelector(episodeSelectorClass);
+
+  if (!episodeSelector) {
+    return;
+  }
+
+  const episodes = episodeSelector.querySelectorAll(titleCardListClass);
+  const episodesToBlur = filterWatchedEpisodes(episodes);
+
+  blurEpisodes(episodesToBlur);
+}
+
+function filterWatchedEpisodes(episodes) {
+  let episodesToBlur = [];
+
+  episodes.forEach((episode) => {
+    const progressElement = episode.querySelector("progress");
+
+    if (!progressElement) {
+      episodesToBlur.push(episode);
+    }
+  });
+
+  // si episodios vistos es igual a la cantidad de episodios eliminar el primer elemento
+  if (episodes.length === episodesToBlur.length) {
+    episodesToBlur.shift();
+  }
+
+  return episodesToBlur;
+}
+
+function blurEpisodes(episodes) {
+  const blurLevel = 10;
+  const blurEffect = `blur(${blurLevel}px)`;
+
+  episodes.forEach((episode) => {
+    episode.style.filter = blurEffect;
+    episode.style.transition = "filter 0.2s";
+
+    episode.addEventListener("mouseover", () => {
+      episode.style.filter = "none";
+    });
+
+    episode.addEventListener("mouseout", () => {
+      episode.style.filter = blurEffect;
+    });
+  });
+}
---
+++ src/scripts/popup.js
@@ -0,0 +1,27 @@
+const checkbox = document.getElementById("toggle-blur");
+
+if (localStorage.getItem(checkbox.name) === "true") {
+  checkbox.checked = true;
+} else if (localStorage.getItem(checkbox.name) === "false") {
+  checkbox.checked = false;
+} else {
+  storeCheckboxValue(); // Store default value (true)
+}
+
+sendBlurStatus();
+
+checkbox.addEventListener("click", () => {
+  storeCheckboxValue();
+  sendBlurStatus();
+});
+
+function storeCheckboxValue() {
+  localStorage.setItem(checkbox.name, checkbox.checked);
+}
+
+function sendBlurStatus() {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var activeTab = tabs[0];
+    chrome.tabs.sendMessage(activeTab.id, {"blur status": checkbox.checked});
+  });
+}
